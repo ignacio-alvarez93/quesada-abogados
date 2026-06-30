76f23f052838cecb819bbb2dc049ef6f07946c31
Nacho1993 <nacho_1_9_9_3@hotmail.com>
2026-06-30 21:55:29 +0200

    refactor(ui): extraer paginación compacta reutilizable

--- frontend/components/listing/__init__.py
+++ frontend/components/listing/__init__.py
@@ -1,4 +1,5 @@
 from .card_item import card_item
+from .compact_pagination_bar import compact_pagination_bar
 from .counter_chips import counter_chips
 from .pagination_bar import pagination_bar
 from .status_chip import DEFAULT_STATUS_MAP, status_chip
@@ -6,6 +7,7 @@ from .status_chip import DEFAULT_STATUS_MAP, status_chip
 __all__ = [
     "DEFAULT_STATUS_MAP",
     "card_item",
+    "compact_pagination_bar",
     "counter_chips",
     "pagination_bar",
     "status_chip",
